386110f0c1d0e98d42875a3e8014f06c2adfa6cf
Maddy Guthridge <hello@maddyguthridge.com>
2025-01-01 15:54:12 +1100

    Fix failing test cases

--- src/routes/api/admin/data/refresh/+server.ts
+++ src/routes/api/admin/data/refresh/+server.ts
@@ -3,7 +3,7 @@ import { dataIsSetUp } from '$lib/server/data/dataDir';
 import { error, json } from '@sveltejs/kit';
 
 export async function POST({ request, cookies }: import('./$types').RequestEvent) {
-  if (await dataIsSetUp()) {
+  if (!await dataIsSetUp()) {
     error(400, 'Data is not set up');
   }
   await validateTokenFromRequest({ request, cookies });

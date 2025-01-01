3d76121c38dfc36507f8f01809277bf337f4dd02
Maddy Guthridge <hello@maddyguthridge.com>
2025-01-01 15:30:12 +1100

    Implement tests for delete and put for files

--- tests/backend/item/file.delete.test.ts
+++ tests/backend/item/file.delete.test.ts
@@ -2,20 +2,27 @@
  * Test cases for uploading files to an item.
  */
 
-import { it, describe, beforeEach } from 'vitest';
-import genTokenTests from '../tokenCase';
+import { it, describe, beforeEach, expect } from 'vitest';
 import type { ApiClient } from '$endpoints';
 import { setup } from '../helpers';
 import fromFileSystem from '../fileRequest';
+import genTokenTests from '../tokenCase';
 
 let api: ApiClient;
 beforeEach(async () => {
   api = (await setup()).api;
+  await api.item([]).file('example.md').post(await fromFileSystem('README.md'));
 });
 
 
 describe('Success', () => {
-  it.todo('Deletes the file');
+  it('Deletes the file', async () => {
+    await expect(api.item([]).file('example.md').delete())
+      .resolves.toStrictEqual({});
+    // Now requesting the file should give a 404
+    await expect(api.item([]).file('example.md').get())
+      .rejects.toMatchObject({ code: 404 });
+  });
 });
 
 describe('401', () => {
@@ -26,6 +33,12 @@ describe('401', () => {
 });
 
 describe('404', () => {
-  it.todo('Errors if the item does not exist');
-  it.todo('Errors if the file does not exist');
+  it('Errors if the item does not exist', async () => {
+    await expect(api.item(['invalid']).file('example.md').delete())
+      .rejects.toMatchObject({ code: 404 });
+  });
+  it('Errors if the file does not exist', async () => {
+    await expect(api.item([]).file('invalid').delete())
+      .rejects.toMatchObject({ code: 404 });
+  });
 });
--- tests/backend/item/file.put.test.ts
+++ tests/backend/item/file.put.test.ts
@@ -2,21 +2,27 @@
  * Test cases for uploading files to an item.
  */
 
-import { it, describe, beforeEach } from 'vitest';
+import { it, describe, beforeEach, expect } from 'vitest';
 import genTokenTests from '../tokenCase';
 import type { ApiClient } from '$endpoints';
 import { setup } from '../helpers';
 import fromFileSystem from '../fileRequest';
+import { readFile } from 'fs/promises';
 
 let api: ApiClient;
 beforeEach(async () => {
   api = (await setup()).api;
-  await api.item([]).file('example.md').put(await fromFileSystem('README.md'));
+  await api.item([]).file('example.md').post(await fromFileSystem('README.md'));
 });
 
 
 describe('Success', () => {
-  it.todo('Updates the file');
+  it('Updates the file', async () => {
+    await api.item([]).file('example.md').put(await fromFileSystem('LICENSE.md'));
+    // Contents should be updated
+    const content = await api.item([]).file('example.md').get().then(buf => buf.toString());
+    expect(content).toStrictEqual(await readFile('LICENSE.md', { encoding: 'utf-8' }));
+  });
 });
 
 describe('401', () => {
@@ -27,6 +33,12 @@ describe('401', () => {
 });
 
 describe('404', () => {
-  it.todo('Errors if the item does not exist');
-  it.todo('Errors if the file does not exist');
+  it('Errors if the item does not exist', async () => {
+    await expect(api.item(['file']).file('file').put(await fromFileSystem('LICENSE.md')))
+      .rejects.toMatchObject({ code: 404 });
+  });
+  it('Errors if the file does not exist', async () => {
+    await expect(api.item([]).file('invalid').put(await fromFileSystem('LICENSE.md')))
+      .rejects.toMatchObject({ code: 404 });
+  });
 });

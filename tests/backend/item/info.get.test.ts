978886232738db29f7f430fa56e2ecce6a182623
Maddy Guthridge <hello@maddyguthridge.com>
2025-01-01 12:16:51 +1100

    Implement tests for getting item info.json

--- tests/backend/item/info.get.test.ts
+++ tests/backend/item/info.get.test.ts
@@ -1,17 +1,47 @@
 /**
  * Test cases for getting item info
  */
+import type { ApiClient } from '$endpoints';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { setup } from '../helpers';
 
-import { describe, it } from 'vitest';
+let api: ApiClient;
+const itemId = ['item'];
+
+beforeEach(async () => {
+  api = (await setup()).api;
+  await api.item(itemId).info.post('My item');
+});
 
 describe('Success', () => {
-  it.todo('Correctly returns info');
+  it('Correctly returns info', async () => {
+    await expect(api.item(itemId).info.get()).resolves.toStrictEqual({
+      name: expect.any(String),
+      shortName: null,
+      description: '',
+      color: expect.toSatisfy(c => /^#[0-9a-fA-F]{6}$/.test(c)),
+      icon: null,
+      banner: null,
+      children: [],
+      filters: [],
+      sections: [],
+      seo: {
+        description: null,
+        keywords: [expect.any(String)],
+      },
+    })
+  });
 });
 
 describe('400', () => {
-  it.todo('Gives an error if data is not set up');
+  it('Gives an error if data is not set up', async () => {
+    await api.debug.clear();
+    await expect(api.item([]).info.get()).rejects.toMatchObject({ code: 400 });
+  });
 })
 
 describe('404', () => {
-  it.todo("Rejects when an item doesn't exist");
+  it("Rejects when an item doesn't exist", async () => {
+    await expect(api.item(['invalid']).info.get()).rejects.toMatchObject({ code: 404 });
+  });
 });

d47b73c5aca3944c39c23fbe55f026f8bdc6da76
Maddy Guthridge <hello@maddyguthridge.com>
2025-02-04 01:12:24 +1100

    Fix imports for @atlaskit/pragmatic-drag-and-drop

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default defineConfig({
+  ssr:{
+    noExternal: [
+      // Workaround for broken import when running production build
+      // https://github.com/atlassian/pragmatic-drag-and-drop/issues/27#issuecomment-2615335498
+      "@atlaskit/pragmatic-drag-and-drop",
+    ]
+  },
   server: {
     port: parseInt(process.env.PORT as string),
     strictPort: true,

45b4765e0262e6268e35551a4bb0e263abf5860c
Maddy Guthridge <hello@maddyguthridge.com>
2025-02-22 21:35:23 +1100

    Make pretty toast

--- src/lib/ui/toast/Toast.svelte
+++ src/lib/ui/toast/Toast.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
   import type { ToastItem } from './toastManager.svelte';
+  import { fly } from 'svelte/transition';
 
   const toast: ToastItem = $props();
 </script>
 
-<div class="toast-item">
+<div class="toast-item" transition:fly={{ x: 200 }}>
   <h3>{toast.title}</h3>
   <p>{toast.message}</p>
 </div>
+
+<style>
+  .toast-item {
+    background-color: rgb(255, 174, 174);
+    border-radius: 10px;
+    padding: 20px 30px;
+
+    & p, & h3 {
+      margin: 0;
+    }
+
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
+  }
+</style>
--- src/lib/ui/toast/ToastDisplay.svelte
+++ src/lib/ui/toast/ToastDisplay.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
+  import { flip } from 'svelte/animate';
   import Toast from './Toast.svelte';
   import { toasts } from './toastManager.svelte';
 </script>
 
 <div id="toast-display">
-  {#each toasts as toast}
-    <Toast {...toast} />
+  {#each toasts as toast (toast.id)}
+    <div animate:flip={{ duration: 200 }}>
+      <Toast {...toast} />
+    </div>
   {/each}
 </div>
+
+<style>
+  #toast-display {
+    position: fixed;
+    top: 85px;
+    right: 20px;
+    z-index: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+</style>

229b6fa16557e9563b44ecd9786f6159b71d2b9d
Maddy Guthridge <hello@maddyguthridge.com>
2025-02-26 22:45:30 +1100

    Update file deletion tests to test for rejection for critical files

--- tests/backend/item/file.delete.test.ts
+++ tests/backend/item/file.delete.test.ts
@@ -3,10 +3,11 @@
  */
 import { it, describe, beforeEach, expect } from 'vitest';
 import type { ApiClient } from '$endpoints';
-import { setup } from '../helpers';
+import { makeItemInfo, setup } from '../helpers';
 import fromFileSystem from '../fileRequest';
 import genTokenTests from '../tokenCase';
 import itemId from '$lib/itemId';
+import { setItemInfo } from '$lib/server/data/item';
 
 let api: ApiClient;
 beforeEach(async () => {
@@ -21,6 +22,29 @@ describe('Success', () => {
     // Now requesting the file should give a 404
     await expect(api.item(itemId.ROOT).file('example.md').get())
       .rejects.toMatchObject({ code: 404 });
+  });
+});
+
+describe('400', () => {
+  it('Rejects requests to delete the readme file', async () => {
+    await expect(api.item(itemId.ROOT).file('README.md').delete())
+      .rejects.toMatchObject({ code: 400 });
+  });
+
+  it.each([
+    { field: 'icon' },
+    { field: 'banner' },
+  ])('Rejects requests to delete $field image files', async ({ field }) => {
+    await api.item(itemId.ROOT).file('image.png')
+      .post(await fromFileSystem('static/minifolio.png'));
+    // Update info to make `image.png` be the chosen file for the given field
+    const info = makeItemInfo({
+      [field]: 'image.png',
+    });
+    await setItemInfo(itemId.ROOT, info);
+    // Now, deleting the file should fail
+    await expect(api.item(itemId.ROOT).file('image.png').delete())
+      .rejects.toMatchObject({ code: 400 });
   });
 });
 

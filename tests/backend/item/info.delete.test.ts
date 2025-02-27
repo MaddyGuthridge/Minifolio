5877dae7fdc281de11e04698bf340d10539a6ea1
Maddy Guthridge <hello@maddyguthridge.com>
2025-02-27 22:57:15 +1100

    Implement test cases for item deletion

--- tests/backend/item/info.delete.test.ts
+++ tests/backend/item/info.delete.test.ts
@@ -2,41 +2,126 @@
  * Test cases for deleting items.
  */
 import type { ApiClient } from '$endpoints';
-import { beforeEach, describe, it } from 'vitest';
-import { setup } from '../helpers';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { makeItemInfo, setup } from '../helpers';
 import genTokenTests from '../tokenCase';
 import itemId from '$lib/itemId';
 
+let api: ApiClient;
+const child = itemId.fromStr('/item');
+
+beforeEach(async () => {
+  api = (await setup()).api;
+  await api.item(child).info.post('My item');
+});
+
 describe('Success', () => {
-  it.todo('Successfully deletes items');
+  it('Successfully deletes items', async () => {
+    await expect(api.item(child).info.delete())
+      .resolves.toStrictEqual({});
 
-  it.todo('Removes child items');
+    // Expect item not found error
+    await expect(api.item(child).info.get())
+    .rejects.toMatchObject({ code: 404 });
+  });
 
-  it.todo('Removes links to this item');
+  it('Removes child items', async () => {
+    const grandchild = itemId.child(child, 'grandchild');
+    await api.item(grandchild).info.post('Grandchild');
+    await expect(api.item(child).info.delete())
+      .resolves.toStrictEqual({});
+    // Grandchild also deleted
+    await expect(api.item(grandchild).info.get())
+    .rejects.toMatchObject({ code: 404 });
+  });
 
-  it.todo('Removes links to child items');
+  it('Removes links to this item', async () => {
+    const other = itemId.fromStr('/other');
+    await api.item(other).info.post('Other item');
 
-  it.todo('Removes this item from the children of its parent');
+    await api.item(other).info.put(makeItemInfo({
+      sections: [
+        {
+          type: 'links',
+          label: 'See also',
+          style: 'chip',
+          items: [child],
+        },
+      ],
+    }));
+
+    await api.item(child).info.delete();
+
+    // Other item info should have link to this item removed
+    await expect(api.item(other).info.get()).resolves.toMatchObject({
+      sections: [
+        expect.objectContaining({
+          items: [],
+          //     ^^^
+          //     Gone
+        }),
+      ]
+    });
+  });
+
+  it('Removes links to child items', async () => {
+    const grandchild = itemId.child(child, 'grandchild');
+    await api.item(grandchild).info.post('Grandchild');
+    const other = itemId.fromStr('/other');
+    await api.item(other).info.post('Other item');
+
+    await api.item(other).info.put(makeItemInfo({
+      sections: [
+        {
+          type: 'links',
+          label: 'See also',
+          style: 'chip',
+          items: [grandchild],
+          // Link to grandchild
+        },
+      ],
+    }));
+    await api.item(child).info.delete();
+
+    // Link to grandchild should be broken by deletion of child
+    await expect(api.item(other).info.get()).resolves.toMatchObject({
+      sections: [
+        expect.objectContaining({
+          items: [],
+          //     ^^^
+          //     Gone
+        }),
+      ]
+    });
+  });
+
+  it('Removes this item from the children of its parent', async () => {
+    await api.item(child).info.delete();
+
+    // Should be gone from root item
+    await expect(api.item(itemId.ROOT).info.get()).resolves.toMatchObject({
+      children: [],
+    });
+  });
 });
 
 describe('401', () => {
-  let api: ApiClient;
-  const id = itemId.fromStr('/item');
-  beforeEach(async () => {
-    api = (await setup()).api;
-    await api.item(id).info.post('My item');
-  });
-
   genTokenTests(
     () => api,
-    api => api.item(id).info.delete(),
+    api => api.item(child).info.delete(),
   );
 });
 
 describe('403', () => {
-  it.todo('Blocks deletion of the root item');
+  it('Blocks deletion of the root item', async () => {
+    await expect(api.item(itemId.ROOT).info.delete())
+      .rejects.toMatchObject({ code: 403 });
+  });
 });
 
 describe('404', () => {
-  it.todo("Rejects if item doesn't exist");
+  it("Rejects if item doesn't exist", async () => {
+    await expect(api.item(itemId.fromStr('invalid')).info.delete())
+      .rejects.toMatchObject({ code: 404 });
+  });
 });

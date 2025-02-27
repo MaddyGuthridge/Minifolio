72a79959d40f238b55ff3c0bd692341ee1f46384
Maddy Guthridge <hello@maddyguthridge.com>
2025-02-27 23:50:54 +1100

    Pre-fill data on login page in dev mode

---
+++ src/lib/devConsts.ts
@@ -0,0 +1,9 @@
+/** Constants useful when running in dev mode */
+
+export const defaultUsername = 'dev';
+export const defaultPassword = 'abc123ABC!';
+
+export default {
+  defaultUsername,
+  defaultPassword,
+};

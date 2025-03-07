074bf8c5b6ce60f4356370a43ce25eeb3c30d1f9
Maddy Guthridge <hello@maddyguthridge.com>
2025-03-08 00:26:12 +1100

    Update DataImage to use UrlSubscription component

--- src/components/base/DataImage.svelte
+++ src/components/base/DataImage.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-  import { subscribeToContent } from '$lib/contentObserver';
-  import { onMount } from 'svelte';
+  import { UrlSubscription } from '$lib/observer';
 
   type Props = {
     url: string;
@@ -9,40 +8,12 @@
 
   const { url, alt }: Props = $props();
 
-  let unsubscribe: (() => void) | undefined = undefined;
-
-  // https://stackoverflow.com/a/9943419/6335363
-  const makeImageUrl = (url: string) => `${url}#${new Date().getTime()}`;
-
-  let imageUrl = $state(makeImageUrl(url));
-
-  function triggerImageReload() {
-    imageUrl = makeImageUrl(url);
-  }
-
-  onMount(() => {
-    // Subscribe and store the unsubscribe callback
-    unsubscribe = subscribeToContent(url, triggerImageReload);
-
-    return () => {
-      // If we need to unsubscribe, do so
-      if (unsubscribe) {
-        unsubscribe();
-      }
-    };
-  });
-
-  // Watch `path`, and re-subscribe when it changes, and reset the imageUrl
-  $effect(() => {
-    if (unsubscribe) {
-      unsubscribe();
-    }
-    unsubscribe = subscribeToContent(url, triggerImageReload);
-    imageUrl = makeImageUrl(url);
-  });
+  let derivedUrl = $state(url);
 </script>
 
-<img src={imageUrl} {alt} />
+<UrlSubscription {url} bind:derivedUrl />
+
+<img src={derivedUrl} {alt} />
 
 <style>
   img {

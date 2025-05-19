ccfaff48ed568d478360ceeb4a4e39d21b6d14a2
Maddy Guthridge <hello@maddyguthridge.com>
2025-05-20 00:09:04 +1000

    Add front-end option to regenerate auth secret

--- src/routes/admin/LogOutAll.svelte
+++ src/routes/admin/LogOutAll.svelte
@@ -7,10 +7,22 @@
     await api().admin.auth.revoke();
     await goto('/admin/login');
   }
+
+  async function regenerateAuthSecret() {
+    await api().admin.auth.regenerateSecret();
+    await goto('/admin/login');
+  }
 </script>
 
 <div>
   <h2>Log out of all sessions</h2>
   <p>This will sign you out on all of your devices (including this one).</p>
   <Button onclick={logOut} mode="warning">Log out of all sessions</Button>
+
+  <h2>Regenerate authentication secret</h2>
+  <p>
+    This will regenerate the secret value used to sign authentication tokens. This will cause all
+    sessions for all users to be invalidated.
+  </p>
+  <Button onclick={regenerateAuthSecret} mode="warning">Regenerate authentication secret</Button>
 </div>

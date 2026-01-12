b81d70bea0863acd71bf6b0e6dcf8b8291de680d
Maddy Guthridge <hello@maddyguthridge.com>
2026-01-12 22:08:59 +1100

    For a tool that can be replaced with the standard library, they're really doing a lot of advertising

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import commonjs from 'vite-plugin-commonjs';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import dotenv from 'dotenv';
-dotenv.config();
+
+process.loadEnvFile('.env');
 
 export default defineConfig({
   ssr:{

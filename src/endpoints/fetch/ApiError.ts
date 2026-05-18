1888b14ce6af84c7a4b7b2d0d99b96ae2d1a7b43
Maddy Guthridge <hello@maddyguthridge.com>
2026-05-18 23:57:18 +1000

    Fetch wrapper: include full response body where possible

--- src/endpoints/fetch/ApiError.ts
+++ src/endpoints/fetch/ApiError.ts
@@ -6,17 +6,20 @@ class ApiError extends Error {
   error: string;
   /** Status code */
   code: number | null;
+  /** Body (if JSON, it is parsed) */
+  body: object | string | null;
 
   /**
    * Custom error class for errors that happen when fetching data
    */
-  constructor(code: number | null, error: string | object) {
+  constructor(code: number | null, error: string | object, body: string | object | null) {
     if (typeof error === 'object') {
       error = JSON.stringify(error);
     }
     super(`ApiError: [${code}] ${error}`);
     this.error = error;
     this.code = code;
+    this.body = body;
   }
 }
 
--- src/endpoints/fetch/response.ts
+++ src/endpoints/fetch/response.ts
@@ -1,18 +1,26 @@
 import ApiError from './ApiError';
 
+function tryJson(text: string) {
+  try {
+    return JSON.parse(text);
+  } catch {
+    return text;
+  }
+}
+
 /** Process a text response, returning the text as a string */
 export async function text(response: Promise<Response>): Promise<string> {
   const res = await response;
   if ([404, 405].includes(res.status)) {
-    throw new ApiError(404, `Error ${res.status} at ${res.url}`);
+    throw new ApiError(404, `Error ${res.status} at ${res.url}`, tryJson(await res.text()));
   }
   const text = await res.text();
   if ([400, 401, 403].includes(res.status)) {
-    throw new ApiError(res.status, text);
+    throw new ApiError(res.status, text, tryJson(text));
   }
   if (![200, 304].includes(res.status)) {
     // Unknown error
-    throw new ApiError(res.status, `Request got status code ${res.status}`);
+    throw new ApiError(res.status, `Request got status code ${res.status}`, tryJson(text));
   }
   return text;
 }
@@ -22,15 +30,15 @@ export async function xml(response: Promise<Response>): Promise<object> {
   // TODO: Fix this coed duplication
   const res = await response;
   if ([404, 405].includes(res.status)) {
-    throw new ApiError(404, `Error ${res.status} at ${res.url}`);
+    throw new ApiError(404, `Error ${res.status} at ${res.url}`, tryJson(await res.text()));
   }
   const text = await res.text();
   if ([400, 401, 403].includes(res.status)) {
-    throw new ApiError(res.status, text);
+    throw new ApiError(res.status, text, tryJson(text));
   }
   if (![200, 304].includes(res.status)) {
     // Unknown error
-    throw new ApiError(res.status, `Request got status code ${res.status}`);
+    throw new ApiError(res.status, `Request got status code ${res.status}`, tryJson(text));
   }
 
   // Import dynamically, to avoid front-end glitches.
@@ -43,37 +51,35 @@ export async function xml(response: Promise<Response>): Promise<object> {
 export async function json(response: Promise<Response>): Promise<object> {
   const res = await response;
   if ([404, 405].includes(res.status)) {
-    throw new ApiError(404, `Error ${res.status} at ${res.url}`);
+    throw new ApiError(404, `Error ${res.status} at ${res.url}`, tryJson(await res.text()));
   }
   if (res.status >= 500) {
     // Unknown error
-    throw new ApiError(res.status, `Request got status code ${res.status}`);
+    throw new ApiError(res.status, `Request got status code ${res.status}`, tryJson(await res.text()));
   }
   // Decode the data
-  // const text = await res.text();
-  // console.log(text);
+  const text = await res.text();
   let json: object;
   try {
-    json = await res.json();
-    // json = JSON.parse(text);
+    json = JSON.parse(text);
   } catch (err) {
     // JSON parse error
     if (err instanceof Error) {
-      throw new ApiError(null, err.message);
+      throw new ApiError(null, err.message, tryJson(text));
     } else {
-      throw new ApiError(null, `Unknown JSON error ${err}`);
+      throw new ApiError(null, `Unknown JSON error ${err}`, tryJson(text));
     }
   }
 
   if ([400, 401, 403].includes(res.status)) {
     // All 400, 401 and 403 errors have an error message
     const message = (json as { message: string }).message;
-    throw new ApiError(res.status, message);
+    throw new ApiError(res.status, message, tryJson(text));
   }
 
   // Got valid data
   // Assign it to a new object, because otherwise it'll fail to match using
-  // `.toStrictEqual`, likely due to some weirdness with Jest
+  // `.toStrictEqual`, likely due to some weirdness with Jest.
   // Seems to be similar to the issues described at these URLs
   // https://github.com/jestjs/jest/issues/8446
   // https://github.com/nktnet1/jewire?tab=readme-ov-file#53-rewire-and-jest
@@ -84,15 +90,15 @@ export async function json(response: Promise<Response>): Promise<object> {
 export async function buffer(response: Promise<Response>): Promise<Uint8Array> {
   const res = await response;
   if ([404, 405].includes(res.status)) {
-    throw new ApiError(404, `Error ${res.status} at ${res.url}`);
+    throw new ApiError(404, `Error ${res.status} at ${res.url}`, tryJson(await res.text()));
   }
   const buf = await res.bytes();
   if ([400, 401, 403].includes(res.status)) {
-    throw new ApiError(res.status, buf);
+    throw new ApiError(res.status, buf, tryJson(await res.text()));
   }
   if (![200, 304].includes(res.status)) {
     // Unknown error
-    throw new ApiError(res.status, `Request got status code ${res.status}`);
+    throw new ApiError(res.status, `Request got status code ${res.status}`, tryJson(await res.text()));
   }
   return buf;
 }

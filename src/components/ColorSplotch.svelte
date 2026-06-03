535386e1c0ca1720592241f5caf46388cbacbfd2
Maddy Guthridge <hello@maddyguthridge.com>
2026-06-03 21:50:58 +1000

    Revamp about page

---
+++ src/components/ColorSplotch.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import { fade } from 'svelte/transition';
+
+  export type Splotch = {
+    color: string,
+    x: string,
+    y: string,
+    spread: string,
+    position?: string,
+  };
+
+  const { color, x, y, spread, position }: Splotch = $props();
+</script>
+
+<div
+  class="dot"
+  style:--color={color}
+  style:--x={x}
+  style:--y={y}
+  style:--spread={spread}
+  style:--position={position}
+  in:fade
+  out:fade
+></div>
+
+<style>
+  .dot {
+    z-index: -1;
+    width: 0;
+    height: 0;
+    position: var(--position);
+    left: var(--x);
+    top: var(--y);
+    box-shadow: 0 0 calc(var(--spread) * 2) var(--spread) var(--color);
+    transition: all 0.5s;
+  }
+
+  @media (prefers-reduced-motion) {
+    .dot {
+      transition: all 0s;
+    }
+  }
+
+  @media (prefers-contrast: more), (prefers-reduced-transparency) {
+    .dot {
+      box-shadow: none;
+    }
+  }
+</style>
